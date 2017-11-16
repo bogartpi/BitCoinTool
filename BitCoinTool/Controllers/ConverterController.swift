b85810e0d7e40f62691ec3c395bb081b784a6253
bogartpi <bogartpimail@gmail.com>
2017-11-16 18:31:36 +0300

    Added ViewController in storyboard

---
+++ BitCoinTool/Controllers/ConverterController.swift
@@ -0,0 +1,19 @@
+//
+//  ConverterController.swift
+//  BitCoinTool
+//
+//  Created by Pavel Bogart on 16/11/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class ConverterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ BitCoinTool/Controllers/PriceController.swift
@@ -0,0 +1,19 @@
+//
+//  PriceController.swift
+//  BitCoinTool
+//
+//  Created by Pavel Bogart on 16/11/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class PriceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}

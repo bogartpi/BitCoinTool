8e803df05fa8a88877b492ce9d22fb6678736887
bogartpi <bogartpimail@gmail.com>
2017-11-16 12:26:06 +0300

    Initial Commit

---
+++ BitCoinToolTests/BitCoinToolTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitCoinToolTests.swift
+//  BitCoinToolTests
+//
+//  Created by Pavel Bogart on 16/11/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import XCTest
+@testable import BitCoinTool
+
+class BitCoinToolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

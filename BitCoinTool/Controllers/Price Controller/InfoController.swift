3d5fd676504cc06c8c1c4cbccca6b0a77a10b2bc
bogartpi <bogartpimail@gmail.com>
2017-11-17 21:49:38 +0300

    Some minor changes in the project.

---
+++ BitCoinTool/Controllers/Price Controller/InfoController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoController.swift
+//  BitCoinTool
+//
+//  Created by Pavel Bogart on 17/11/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class InfoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- BitCoinTool/Controllers/Price Controller/PriceController.swift
+++ BitCoinTool/Controllers/Price Controller/PriceController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class PriceController: UIViewController {
 
+    private let segueInfoView = "SegueInfoView"
+    private let segueCurrencyView = "SegueCurrencyView"
+    private let segueSettingsView = "SegueSettingsView"
+    private let segueAddCurrencyView = "SegueAddCurrency"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
 
     }
 

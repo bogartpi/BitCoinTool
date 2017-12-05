fb1e61bf715ed9ee57b699c3fc1ab257344ad5e9
bogartpi <bogartpimail@gmail.com>
2017-12-06 10:06:40 +1300

    Added SafeAreaLayout

---
+++ BitCoinTool/Helpers/Constraints.swift
@@ -0,0 +1,11 @@
+//
+//  Constraints.swift
+//  BitCoinTool
+//
+//  Created by Pavel Bogart on 06/12/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ BitCoinTool/Helpers/SafeAreaLayout.swift
@@ -0,0 +1,36 @@
+//
+//  SafeAreaLayout.swift
+//  BitCoinTool
+//
+//  Created by Pavel Bogart on 06/12/2017.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.topAnchor
+        }
+        return topAnchor
+    }
+    var safeLeftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.leftAnchor
+        }
+        return leftAnchor
+    }
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.bottomAnchor
+        }
+        return bottomAnchor
+    }
+    var safeRightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.rightAnchor
+        }
+        return rightAnchor
+    }
+}
